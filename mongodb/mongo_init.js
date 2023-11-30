5810fd34d8756b11c3d6f4f8002fbe96b7c5e702
Boyia <1353999534@qq.com>
2023-11-30 16:21:29 +0800

    docker compose

--- mongodb/mongo_init.js
+++ mongodb/mongo_init.js
@@ -1,14 +1,19 @@
+db = db.getSiblingDB("scrap");
 // create a user
+
 db.runCommand(
-    {
-      createUser: "boyia",
-      pwd:'002780', 
-       roles: ["readWrite","dbAdmin"], 
-      });
-
-//create a db
-use scrap
-
+  {
+    createUser: "boyia",
+    pwd:'002780', 
+     roles: [
+      {
+        "role" : "dbAdmin",
+        //create a db
+        "db" : "scrap"
+      }
+    
+      ], 
+    });
 //create a collection 
 db.createCollection('pollution')
 
